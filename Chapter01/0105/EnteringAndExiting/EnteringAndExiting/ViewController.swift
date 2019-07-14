e82dcc0718964315c50cbcd9d2ab9b7414f4d136
CoderDream <coderdream@gmail.com>
2019-07-14 19:10:34 +0800

    ebook version20190714

--- Chapter01/0105/EnteringAndExiting/EnteringAndExiting/ViewController.swift
+++ Chapter01/0105/EnteringAndExiting/EnteringAndExiting/ViewController.swift
@@ -11,72 +11,122 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    override func viewDidLoad() {
+//
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//        print("viewDidLoad")
+//
+//        let center = NotificationCenter.default
+//
+//        let didBecomeActive = #selector(
+//           // UIApplicationDelegate.
+//            applicationDidBecomeActive(_:)
+//        )
+//
+//        let willEnterForeground = #selector(
+//            //UIApplicationDelegate.
+//            applicationWillEnterForeground(_:)
+//        )
+//
+//        let willResignActive = #selector(
+//            //UIApplicationDelegate.
+//            applicationWillResignActive(_:)
+//        )
+//
+//        let didEnterBackground = #selector(
+//            //UIApplicationDelegate.
+//            applicationDidEnterBackground(_:)
+//        )
+//
+//        center.addObserver(self,
+//                           selector: didBecomeActive,
+//                           name: UIApplication.didBecomeActiveNotification,
+//                           object: nil)
+//
+//        center.addObserver(self,
+//                           selector: willEnterForeground,
+//                           name: UIApplication.willEnterForegroundNotification,
+//                           object: nil)
+//
+//        center.addObserver(self,
+//                           selector: willResignActive,
+//                           name: UIApplication.willResignActiveNotification,
+//                           object: nil)
+//
+//        center.addObserver(self,
+//                           selector: didEnterBackground,
+//                           name: UIApplication.didEnterBackgroundNotification,
+//                           object: nil)
+//    }
+//
+//    @objc func applicationDidBecomeActive(_ notification : Notification) {
+//        print("Application became active")
+//    }
+//
+//    @objc func applicationDidEnterBackground(_ notification : Notification) {
+//        print("Application entered background - unload textures!")
+//    }
+//
+//    @objc func applicationWillEnterForeground(_ notification : Notification) {
+//        print("Application will enter foreground - reload " +
+//            "any textures that were unloaded")
+//    }
+//
+//    @objc func applicationWillResignActive(_ notification : Notification) {
+//        print("Application will resign active - pause the game now!")
+//    }
+//
+//    deinit {
+//        // Remove this object from the notification center
+//        NotificationCenter.default.removeObserver(self)
+//    }
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("viewDidLoad")
-        
         let center = NotificationCenter.default
-
         let didBecomeActive = #selector(
-           // UIApplicationDelegate.
-            applicationDidBecomeActive(_:)
+            ViewController.applicationDidBecomeActive(notification:)
         )
-
         let willEnterForeground = #selector(
-            //UIApplicationDelegate.
-            applicationWillEnterForeground(_:)
+            ViewController.applicationWillEnterForeground(notification:)
         )
-
         let willResignActive = #selector(
-            //UIApplicationDelegate.
-            applicationWillResignActive(_:)
+            ViewController.applicationWillResignActive(notification:)
         )
-
         let didEnterBackground = #selector(
-            //UIApplicationDelegate.
-            applicationDidEnterBackground(_:)
+            ViewController.applicationDidEnterBackground(notification:)
         )
-
         center.addObserver(self,
                            selector: didBecomeActive,
                            name: UIApplication.didBecomeActiveNotification,
                            object: nil)
-
         center.addObserver(self,
                            selector: willEnterForeground,
                            name: UIApplication.willEnterForegroundNotification,
                            object: nil)
-
         center.addObserver(self,
                            selector: willResignActive,
                            name: UIApplication.willResignActiveNotification,
                            object: nil)
-
         center.addObserver(self,
                            selector: didEnterBackground,
                            name: UIApplication.didEnterBackgroundNotification,
                            object: nil)
     }
-    
-    @objc func applicationDidBecomeActive(_ notification : Notification) {
+    @objc func applicationDidBecomeActive(notification : Notification) {
         print("Application became active")
     }
-
-    @objc func applicationDidEnterBackground(_ notification : Notification) {
+    @objc func applicationDidEnterBackground(notification : Notification) {
         print("Application entered background - unload textures!")
     }
-
-    @objc func applicationWillEnterForeground(_ notification : Notification) {
+    @objc func applicationWillEnterForeground(notification : Notification) {
         print("Application will enter foreground - reload " +
             "any textures that were unloaded")
     }
-
-    @objc func applicationWillResignActive(_ notification : Notification) {
+    @objc func applicationWillResignActive(notification : Notification) {
         print("Application will resign active - pause the game now!")
     }
-    
     deinit {
         // Remove this object from the notification center
         NotificationCenter.default.removeObserver(self)
