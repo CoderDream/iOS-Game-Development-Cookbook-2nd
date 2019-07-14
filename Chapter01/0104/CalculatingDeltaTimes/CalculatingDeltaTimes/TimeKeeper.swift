ba44f8b57c0ce0e922c5db3014397383b9cd96bf
CoderDream <coderdream@gmail.com>
2019-07-14 14:42:25 +0800

    1.4 计算屏幕的帧刷新时间 20190714

--- Chapter01/0104/CalculatingDeltaTimes/CalculatingDeltaTimes/TimeKeeper.swift
+++ Chapter01/0104/CalculatingDeltaTimes/CalculatingDeltaTimes/TimeKeeper.swift
@@ -17,18 +17,22 @@ class TimeKeeper {
     // BEGIN deltatime_calculating
     func update(currentTime: Double) {
         // Calculate the time since this method was last called
+        // 计算从上次帧刷新到本次帧刷新之间的时间差
         let deltaTime = currentTime - lastFrameTime
         
         // Move at 3 units per second
+        // 移动速度为：每秒三个单位
         let movementSpeed = 3.0
         
         // Multiply by deltaTime to work out how far
         // an object needs to move this frame
+        // 用 deltaTime 乘以移动速度即可得到这一帧应该移动多少距离
+        // 即公式：时间×速度=运动距离
         someMovingObject.move(distance: movementSpeed * deltaTime)
         
         // Set last frame time to current time, so that
-        // we can calculate the delta time when we're next
-        // called
+        // we can calculate the delta time when we're next called
+        // 将当前时间保存到 lastFrameTime，以便下次刷新时计算每帧耗时
         lastFrameTime = currentTime
     }
     // END deltatime_calculating
