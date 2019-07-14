3cb528e5d29729124ef6255c08c8c76e304d1a9d
CoderDream <coderdream@gmail.com>
2019-07-14 20:03:59 +0800

    添加注释 20190714

--- Chapter01/0110/WorkingWithClosures/WorkingWithClosures/ViewController.swift
+++ Chapter01/0110/WorkingWithClosures/WorkingWithClosures/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class GameObject {
     // define a type of closure that takes a single GameObject
     // as a parameter and returns nothing
+    // 定义一个闭包，该闭包有一个 GameObject 参数，返回值为空
     var onCollision: ((GameObject) -> Void)?
 }
 
@@ -23,13 +24,17 @@ class ViewController: UIViewController {
         print("viewDidLoad")
         
         // Create two objects for this example
+        // 创建两个 GameObject 对象
         let car = GameObject()
         let brickWall = GameObject()
         // Provide code to run when the car hits any another object
+        // 用一段代码来赋值给 car 对象的 onCollision 属性，以便 car 对象与其他对象碰撞时能够知晓该段代码
         car.onCollision = { (objectWeHit) in
             print("Car collided with \(objectWeHit)")
         }
         // later, when a character collides:
+        // 最后，当碰撞发生时：
+        // 注意 ? 的用法，这表明只有 onCollision 不为空时该语句才会被执行
         car.onCollision?(brickWall) // note the ? - this means that
         // the code will only run if onCollision is not nil
     }
