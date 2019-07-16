6169dfff61c7cd19fbd64e37a0e90c76a2c92ef8
CoderDream <coderdream@gmail.com>
2019-07-16 10:11:21 +0800

    update comments

--- Chapter03/03017/Steering/Steering/ViewController.swift
+++ Chapter03/03017/Steering/Steering/ViewController.swift
@@ -29,13 +29,15 @@ class ViewController: UIViewController {
             }
             
             // Maximum steering left is -50 degrees, maximum steering right is 50 degrees
-            // 
+            // 最大转向角度，向左为-50度，向右为50度
             let maximumSteerAngle = 50.0
             
             // When in landscape,
+            // 在横屏中
             let rotationAngle = motion.attitude.pitch * 180.0 / .pi
             
             // -1.0 = hard left, 1.0 = hard right
+            // 用-1.-表示左满舵，1.0表示右满舵
             var steering = 0.0
             
             let orientation = UIApplication.shared.statusBarOrientation
@@ -47,6 +49,7 @@ class ViewController: UIViewController {
             }
             
             // Limit the steering to between -1.0 and 1.0
+            // 限制转向值在 -1.0 到 1.0 之间
             steering = fmin(steering, 1.0)
             steering = fmax(steering, -1.0)
             
