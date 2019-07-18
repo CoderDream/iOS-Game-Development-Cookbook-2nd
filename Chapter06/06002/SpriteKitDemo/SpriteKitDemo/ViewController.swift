a375392a04399eef886b91da9295a0bc568b7b7b
CoderDream <coderdream@gmail.com>
2019-07-18 10:55:36 +0800

    6.2 创建Sprite Kit视图 20190718

---
+++ Chapter06/06002/SpriteKitDemo/SpriteKitDemo/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SpriteKitDemo
+//
+//  Created by CoderDream on 2019/7/18.
+//  Copyright © 2019 CoderDream. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+        
+    override func loadView() {
+        print("loadView")
+        self.view = SKView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("viewDidLoad")
+        if let spriteView = self.view as? SKView {
+            print("SKView")
+            spriteView.showsDrawCount = true
+            spriteView.showsFPS = true
+            spriteView.showsNodeCount = true
+        }
+    }
+
+
+}
+
